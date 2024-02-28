167ec9f221e91eb00811678d8bff71fbe6415c1a
AlexMason94 <alejandro2000barriosvallejos13@gmail.com>
2024-02-27 21:24:17 -0400

    primeros codigos en prolog

---
+++ familia.pl
@@ -0,0 +1,43 @@
+% toda la familia
+
+cristiano.
+mariel.
+juan.
+batista.
+cristal.
+mario.
+alejandra.
+adrian.
+luciano.
+alvaro.
+
+% todos los hombre
+esHombre(cristiano).
+esHombre(juan).
+esHombre(batista).
+esHombre(mario).
+esHombre(adrian).
+esHombre(luciano).
+esHombre(alvaro).
+
+% todas las mujeres
+esMujer(cristal).
+esMujer(mariel).
+esMujer(alejandra).
+
+% hijos de cristiano
+esHijoOhija(cristiano,juan).
+esHijoOhija(cristiano,batista).
+
+esHijoOhija(mariel,batista).
+esHijoOhija(mariel,cristal).
+esHijoOhija(cristal,adrian).
+esHijoOhija(batista,adrian).
+esHijoOhija(cristal,luciano).
+esHijoOhija(batista,luciano).
+esHijoOhija(cristal,alejandra).
+esHijoOhija(batista,alejandra).
+esHijoOhija(alejandra,alvaro).
+esHijoOhija(mario,alvaro).
+
+
