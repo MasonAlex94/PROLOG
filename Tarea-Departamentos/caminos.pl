0a6f1124b20389c21c0ba7543e696ba2e25c605d
AlexMason94 <alejandro2000barriosvallejos13@gmail.com>
2024-03-17 19:38:10 -0400

    Tarea Syntaxis de PROLOG parte 2 completada con recursividad

--- Tarea-Departamentos/caminos.pl
+++ Tarea-Departamentos/caminos.pl
@@ -53,75 +53,16 @@ pertenece_a(ciudad_de_cochabamba, cochabamba).
 pertenece_a(sucre, chuquisaca).
 pertenece_a(ciudad_de_tarija, tarija).
 
-% Conexiones ciudades
-conectado_directamente(cobija, apolo).
-conectado_directamente(apolo, cobija).
-conectado_directamente(riberalta, cobija).
-conectado_directamente(ciudad_de_la_paz, apolo).
-conectado_directamente(apolo, ciudad_de_la_paz).
-conectado_directamente(ciudad_de_la_paz, oruro).
-conectado_directamente(oruro, ciudad_de_la_paz).
-conectado_directamente(ciudad_de_la_paz, cochabamba).
-conectado_directamente(cochabamba, ciudad_de_la_paz).
-conectado_directamente(ciudad_de_la_paz, trinidad).
-conectado_directamente(ciudad_de_oruro, uyuni).
-conectado_directamente(ciudad_de_oruro, sucre).
-conectado_directamente(sucre, ciudad_de_cochabamba).
-conectado_directamente(ciudad_de_cochabamba, sucre).
-conectado_directamente(ciudad_de_santacruz, cochabamba).
-conectado_directamente(uyuni, ciudad_de_tarija).
-conectado_directamente(ciudad_de_tarija, uyuni).
-conectado_directamente(ciudad_de_tarija, villamontes).
-conectado_directamente(villamontes, ciudad_de_tarija).
-conectado_directamente(ciudad_de_santacruz, san_jose_de_chiquitos).
-conectado_directamente(puerto_suarez, san_jose_de_chiquitos).
-conectado_directamente(san_jose_de_chiquitos, puerto_suarez).
-conectado_directamente(ciudad_de_santa_cruz, trinidad).
-conectado_directamente(ciudad_de_tarija, sucre).
-
-% conexiones directas entre departamentos
-conectado_directamente(la_paz, pando).
-conectado_directamente(la_paz, oruro).
-conectado_directamente(la_paz, cochabamba).
-conectado_directamente(la_paz, beni).
-conectado_directamente(pando, la_paz).
-conectado_directamente(pando, beni).
-conectado_directamente(beni, la_paz).
-conectado_directamente(beni, pando).
-conectado_directamente(beni, cochabamba).
-conectado_directamente(beni, santa_cruz).
-conectado_directamente(cochabamba, la_paz).
-conectado_directamente(cochabamba, beni).
-conectado_directamente(cochabamba, santa_cruz).
-conectado_directamente(cochabamba, oruro).
-conectado_directamente(cochabamba, potosi).
-conectado_directamente(cochabamba, chuquisaca).
-conectado_directamente(oruro, la_paz).
-conectado_directamente(oruro, cochabamba).
-conectado_directamente(oruro, potosi).
-conectado_directamente(oruro, chuquisaca).
-conectado_directamente(potosi, oruro).
-conectado_directamente(potosi, cochabamba).
-conectado_directamente(potosi, chuquisaca).
-conectado_directamente(potosi, tarija).
-conectado_directamente(chuquisaca, santa_cruz).
-conectado_directamente(chuquisaca, cochabamba).
-conectado_directamente(chuquisaca, potosi).
-conectado_directamente(chuquisaca, tarija).
-conectado_directamente(tarija, chuquisaca).
-conectado_directamente(tarija, potosi).
-
-
-% Consulta para encontrar a qué ciudades se puede viajar
+% Regla para encontrar ciudades destino desde un punto de origen (recursiva)
 ciudades_destino_desde(CiudadOrigen, CiudadDestino) :-
     conectado_directamente(CiudadOrigen, CiudadDestino).
+ciudades_destino_desde(CiudadOrigen, CiudadDestino) :-
+    conectado_directamente(CiudadOrigen, CiudadIntermedia),
+    ciudades_destino_desde(CiudadIntermedia, CiudadDestino).
 
-
-% Consulta para encontrar desde qué ciudades se puede llegar directamente a 'sucre'
+% Regla para encontrar desde qué ciudades se puede llegar directamente a 'sucre' (recursiva)
 ciudades_con_acceso_directo_a_sucre(CiudadOrigen) :-
     conectado_directamente(CiudadOrigen, sucre).
-
-
-
-
-
+ciudades_con_acceso_directo_a_sucre(CiudadOrigen) :-
+    conectado_directamente(CiudadOrigen, CiudadIntermedia),
+    ciudades_con_acceso_directo_a_sucre(CiudadIntermedia).
